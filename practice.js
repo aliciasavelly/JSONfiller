04c3c23861c63549694dbf7da7ab62bc4ce731b4
aliciasavelly <alicia.savelly@gmail.com>
2017-08-06 22:11:46 -0700

    fix json

--- practice.js
+++ practice.js
@@ -12,10 +12,65 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open('GET', 'https://aliciasavelly.github.io/JSONfiller/');
 xmlhttp.send('');
 
+count = 0;
+
 function interact(arr) {
-  console.log(arr);
-  console.log("-----");
   arr.forEach( el => {
-    console.log(el);
+    helper(el, "body");
   });
+}
+
+function helper(arr) {
+  console.log("----------");
+  // let parent;
+  // if (arr["node"] == "root") {
+  //   parent = "body";
+  // }
+
+  if (arr["node"] == "text") {
+    document.getElementByTagName(parent).innerHTML = arr["text"];
+    return;
+  } else if (arr["node"] == "element" && parent == "body") {
+    let newChild = document.createElement(arr["tag"]);
+    console.log(newChild);
+    for (let attr in arr["attr"]) {
+      newChild.setAttribute(attr, arr["attr"][attr]);
+    }
+    document.getElementById("body").appendChild(newChild);
+  }
+  // console.log(arr);
+  // console.log(arr["node"] == "root");
+  // console.log(parent);
+  // console.log(parent);
+  // if (parent == undefined) {
+  //   return;
+  // }
+
+  // for (let element in arr) {
+  //   interact(element);
+  // }
+  // console.log(arr["child"]);
+  count += 1;
+  if (count > 10) {
+    return count;
+  }
+
+  // console.log(arr["child"]);
+  if (arr["child"]) {
+    console.log("42");
+    console.log(arr["child"]);
+    arr["child"].forEach( el => {
+      helper(el, arr["tag"]);
+    });
+  } else {
+    console.log("41");
+    console.log(arr);
+    // for (let el in arr) {
+    //   console.log(el);
+    //   helper(el, parent);
+    // }
+    arr.forEach( el => {
+      helper(el, parent);
+    });
+  }
 }
